1850c402477190f9b0bc0025f6412455d45c6dcc
FELIPE R DA SILVA <mtz.dev.si@gmail.com>
2025-05-14 21:12:31 -0300

    configurando projeto react para deploy com github pages

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/financial_portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
